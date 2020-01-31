dd528eb213c7889127d620b40ac74ab2bad042ec
quangdz1704 <60279723+quangdz1704@users.noreply.github.com>
2020-01-31 20:06:14 +0700

    add all

---
+++ javascript/shiftUnshift.js
@@ -0,0 +1,20 @@
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift(); //remove first item of arr
+                //pop() remove last item
+
+// Only change code below this line.
+myArray.unshift(["Paul", 35]);//add first
+                              //push() add last
+console.log(myArray);
+
+var test = {
+    "ten": "hello",
+    "tuoi": 30
+}
